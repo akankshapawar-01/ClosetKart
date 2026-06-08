56625154057f62f3547fea9537ca0bfd717d7248
akankshapawar-01 <pawarakanksha1101@gmail.com>
2026-06-08 21:35:25 +0530

    Day 4: Added Auth Middleware and Product Model

---
+++ backend/models/Product.js
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+
+const reviewSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  name: { type: String, required: true },
+  rating: { type: Number, required: true },
+  comment: { type: String, required: true },
+}, { timestamps: true });
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  reviews: [reviewSchema],
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  numReviews: {
+    type: Number,
+    default: 0,
+  },
+}, { timestamps: true });
+
+export default mongoose.model('Product', productSchema);
