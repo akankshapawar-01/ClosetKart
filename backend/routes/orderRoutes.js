e128b0178433d405cc486e5b69271a85734b8613
akankshapawar-01 <pawarakanksha1101@gmail.com>
2026-06-10 13:26:25 +0530

    Day 6: Added Order Model, Order Controller and Order Routes~

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import {
+  createOrder,
+  getMyOrders,
+  getOrderById,
+  getAllOrders,
+  updateOrderToDelivered,
+} from '../controllers/orderController.js';
+import { protect, adminOnly } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+// User routes
+router.post('/', protect, createOrder);
+router.get('/myorders', protect, getMyOrders);
+router.get('/:id', protect, getOrderById);
+
+// Admin routes
+router.get('/', protect, adminOnly, getAllOrders);
+router.put('/:id/deliver', protect, adminOnly, updateOrderToDelivered);
+
+export default router;
